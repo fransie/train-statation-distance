4022e03a7f63c3d1a0a28ab3b052aa617d8ea650
Franzi <62101565+fransie@users.noreply.github.com>
2023-02-02 09:32:09 +0100

    Create layered project structure

---
+++ IntegrationTests/ControllerTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace IntegrationTests
+{
+    public class IntegrationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
