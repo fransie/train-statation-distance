14a86bcc0bba7f2e929f9e70e8c4a13da2965ef1
Franzi <62101565+fransie@users.noreply.github.com>
2023-02-02 16:37:37 +0100

    Implement DistanceCalculationService

---
+++ BusinessLogic/Service/DistanceCalculationService.cs
@@ -0,0 +1,59 @@
+using System;
+using BusinessLogic.Model;
+using Microsoft.Extensions.Logging;
+
+namespace BusinessLogic.Service;
+
+public class DistanceCalculationService : IDistanceCalculationService
+{
+    private readonly ITrainStationRepository _trainStationRepository;
+    private readonly ILogger<IDistanceCalculationService> _logger;
+
+    public DistanceCalculationService(ITrainStationRepository trainStationRepository,
+        ILogger<IDistanceCalculationService> logger)
+    {
+        _trainStationRepository = trainStationRepository;
+        _logger = logger;
+    }
+
+    public DistanceCalculation CalculateDistance(string from, string to)
+    {
+        TrainStation fromTrainStation;
+        TrainStation toTrainStation;
+        try
+        {
+            fromTrainStation = _trainStationRepository.GetByDs100Code(from.ToUpper());
+            toTrainStation = _trainStationRepository.GetByDs100Code(to.ToUpper());
+        }
+        catch (ArgumentException _)
+        {
+            _logger.LogWarning($"Either \"{from}\" or \"{to}\" is an invalid DS100Code.");
+            return null;
+        }
+
+        var distance = CalculateHaversineDistanceInKm(fromTrainStation, toTrainStation);
+
+        var distanceCalculation = new DistanceCalculation
+        {
+            From = fromTrainStation.Name,
+            To = toTrainStation.Name,
+            Distance = distance,
+            Unit = "km"
+        };
+        return distanceCalculation;
+    }
+
+
+    // The Haversine Formula calculates the distance between to point on the earth.
+    // This implementation is heavily inspired by https://de.martech.zone/calculate-great-circle-distance/.
+    private static int CalculateHaversineDistanceInKm(TrainStation from, TrainStation to)
+    {
+        var theta = from.Longitude - to.Longitude;
+        var distance = 60 * 1.1515 * (180 / Math.PI) * Math.Acos(
+            Math.Sin(from.Latitude * (Math.PI / 180)) * Math.Sin(to.Latitude * (Math.PI / 180)) +
+            Math.Cos(from.Latitude * (Math.PI / 180)) * Math.Cos(to.Latitude * (Math.PI / 180)) *
+            Math.Cos(theta * (Math.PI / 180))
+        );
+        return (int)Math.Round(distance * 1.609344);
+    }
+}
--- BusinessLogicTests/DistanceCalculationServiceTests.cs
+++ BusinessLogicTests/DistanceCalculationServiceTests.cs
@@ -1,33 +1,54 @@
 using BusinessLogic.Model;
 using BusinessLogic.Service;
 using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using NUnit.Framework;
 
 namespace BusinessLogicTests;
 
-public class TrainStationServiceTests
+public class DistanceCalculationServiceTests
 {
-    [TestCase("FF", "Frankfurt(Main)Hbf", "BLS","Berlin Hbf", 423)]
-    [TestCase("ff", "Frankfurt(Main)Hbf", "bls","Berlin Hbf", 423)]
-    [TestCase("KK", "Köln Hbf", "KB", "Bonn Hbf", 25)]
-    public void CalculateDistance_WithValidCodes_ReturnsCorrectDistance(string from, string fromName, string to, string toName, int expectedDistance)
+    private readonly ITrainStationRepository _trainStationRepository = Substitute.For<ITrainStationRepository>();
+
+    [TestCase("FF", "BLS", 423)]
+    [TestCase("ff", "bls", 423)]
+    public void CalculateDistance_WithValidCodes_ReturnsCorrectDistance(string from, string to, int expectedDistance)
     {
         // given
         var sut = CreateSut();
+        const string frankfurtHbf = "Frankfurt(Main)Hbf";
+        const string berlinHbf = "Berlin Hbf";
         var expectedDistanceCalculation = new DistanceCalculation
         {
-            From = fromName,
-            To = toName,
+            From = "Frankfurt(Main)Hbf",
+            To = berlinHbf,
             Distance = expectedDistance,
             Unit = "km"
         };
-        
+        _trainStationRepository.GetByDs100Code(from).Returns(
+            new TrainStation
+            {
+                Ds100Code = from,
+                Name = frankfurtHbf,
+                Longitude = 8.663789,
+                Latitude = 50.107145
+            });
+        _trainStationRepository.GetByDs100Code(to).Returns(
+            new TrainStation
+            {
+                Ds100Code = from,
+                Name = berlinHbf,
+                Longitude = 13.369545,
+                Latitude = 52.525592
+            });
+
         // when
         var distanceCalculation = sut.CalculateDistance(from, to);
-        
+
         // then
         distanceCalculation.Should().BeEquivalentTo(expectedDistanceCalculation);
-
     }
 
     [Test]
@@ -35,18 +56,19 @@ public class TrainStationServiceTests
     {
         // given
         var sut = CreateSut();
-        const string invalidCode = "invalid";
-        
+        const string invalidCode = "INVALID";
+        _trainStationRepository.GetByDs100Code(invalidCode).Throws(new ArgumentException());
+
         // when
         var distanceCalculation = sut.CalculateDistance(invalidCode, invalidCode);
-        
+
         // then
         distanceCalculation.Should().BeNull();
     }
 
-    
+
     private IDistanceCalculationService CreateSut()
     {
-        throw new NotImplementedException();
+        return new DistanceCalculationService(_trainStationRepository, Substitute.For<ILogger<IDistanceCalculationService>>());
     }
 }
